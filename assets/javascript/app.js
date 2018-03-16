a73db066e2c1e4b77ae975532cfa539644ce5573
Josh <jhint2424@gmail.com>
2018-03-15 22:38:07 -0700

    adding click function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
 
   // Initialize Firebase
-  var config = {
+  var config = 
+  {
     apiKey: "AIzaSyCtkkwE8fIbfMujp8Um8aKiATdunzNTATE",
     authDomain: "train-scheduler-1c05f.firebaseapp.com",
     databaseURL: "https://train-scheduler-1c05f.firebaseio.com",
@@ -8,6 +9,23 @@
     storageBucket: "train-scheduler-1c05f.appspot.com",
     messagingSenderId: "61675721712"
   };
+
   firebase.initializeApp(config);
   
   var database = firebase.database();
+
+  var name;
+  var dest;
+  var time;
+  var freq;
+  
+  $('#run-search').on('click', function(e) 
+  {
+    e.preventDefault();
+    name = $('#train-name').val();
+    role = $('#train-dest').val();
+    start = $('#train-time').val();
+    rate = $('#train-freq').val();
+    console.log(name, dest, time, freq)
+    console.log(moment().format("HH:MM a"));
+});
