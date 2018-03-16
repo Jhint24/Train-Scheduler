5772bf161eee1b165f85d3dc2a151c44100a73c0
Josh <jhint2424@gmail.com>
2018-03-15 22:50:48 -0700

    adding js functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,19 +13,35 @@
   firebase.initializeApp(config);
   
   var database = firebase.database();
-
+  //add var newTrain={}
   var name;
   var dest;
   var time;
   var freq;
-  
-  $('#run-search').on('click', function(e) 
+  //add click functions
+  $('#run-submit').on('click', function(e) 
   {
     e.preventDefault();
     name = $('#train-name').val();
-    role = $('#train-dest').val();
-    start = $('#train-time').val();
-    rate = $('#train-freq').val();
+    dest = $('#train-dest').val();
+    time = $('#train-time').val();
+    freq = $('#train-freq').val();
     console.log(name, dest, time, freq)
     console.log(moment().format("HH:MM a"));
+    //upload to database
+    database.ref().push(
+    {
+        name: name,
+        dest: dest,
+        time: time,
+        freq: freq
+    });
+        //clear forms
+      $('#train-name').val('');
+      $('#train-dest').val('');
+      $('#train-time').val('');
+      $('#train-freq').val('');
+  
+    
+        
 });
